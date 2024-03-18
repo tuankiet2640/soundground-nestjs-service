ddc2d4c0a9ea33980b8a41eae13879ff27b112a9
Bui Van Tuan Kiet <tuankiet2640@gmail.com>
2024-03-18 23:11:49 +0700

    remove some unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { AppService } from "./app.service";
 import { AudioModule } from "./audio/audio.module";
 import { TrackController } from "./controller/track.controller";
 import { TrackService } from "./service/track.service";
@@ -23,18 +22,18 @@ import { AppUserController } from "./controller/app-user.controller";
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USERNAME'),
+        host: configService.get<string>("DATABASE_HOST"),
+        port: configService.get<number>("DATABASE_PORT"),
+        username: configService.get<string>("DATABASE_USERNAME"),
         password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
+        database: configService.get<string>("DATABASE_NAME"),
         entities: [Track, AppUser, Playlist, Comment],
-        synchronize: true,
-      }),
+        synchronize: true
+      })
     }),
-    TypeOrmModule.forFeature([Track, AppUser, Playlist, Comment]),
+    TypeOrmModule.forFeature([Track, AppUser, Playlist, Comment])
   ],
   controllers: [TrackController, PlaylistController, AppUserController],
-  providers: [AppService, TrackService, PlaylistService, AppUserService],
+  providers: [TrackService, PlaylistService, AppUserService]
 })
 export class AppModule {}
